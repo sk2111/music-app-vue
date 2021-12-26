74394f8e258e82bbc8454fc85a973906a646b139
Sathishkumar Thangavel <--global>
2021-12-26 09:59:12 +0530

    feat: :sparkles: added navigation for home & manage components

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
+import Manage from '@/views/Manage.vue';
 
 const routes = [
   {
+    name: 'home',
     path: '/',
-    name: 'Home',
     component: Home,
   },
   {
+    name: 'about',
     path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: About,
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' },
+  },
+  {
+    name: 'manage',
+    path: '/manage-music',
+    component: Manage,
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
 });
 
 export default router;
